2738396a6a207f3181aabb23c789a41c204d4b59
wujinsheng <wujs@neotv.me>
2016-12-22 20:54:23 +0800

    -Update

---
+++ src/main/java/cn/dianjingquan/api/controller/MatchBuildController.java
@@ -0,0 +1,16 @@
+package cn.dianjingquan.api.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by tommy on 2016-12-22.
+ * ees
+ * cn.dianjingquan.api.controller.MatchBuildController
+ */
+@RestController
+@RequestMapping("/v1/match")
+@Api(value = "赛事详情 API",description = "赛事详情 API")
+public class MatchBuildController {
+}
