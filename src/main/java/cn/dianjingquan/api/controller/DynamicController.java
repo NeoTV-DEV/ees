58d422607114b0b8e85a04d6cc6f9a1a17ed4c9c
wujinsheng <wujs@neotv.me>
2017-01-05 19:15:38 +0800

    -update

--- src/main/java/cn/dianjingquan/api/controller/DynamicController.java
+++ src/main/java/cn/dianjingquan/api/controller/DynamicController.java
@@ -14,28 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
  * cn.dianjingquan.api.controller.DynamicController
  */
 @RestController
-@RequestMapping("/v1/match/dynamic")
 @Api(value = "赛事动态 API", description = "赛事动态 API")
 public class DynamicController {
-    @ApiOperation(value = "获取赛事动态列表", notes = "", response = DynamicList.class, consumes = "application/json")
+    @ApiOperation(value = "获取赛事动态列表", notes = "", response = DynamicList.class, produces = "application/json", consumes = "application/json")
     @ApiImplicitParams({
+            @ApiImplicitParam(name = "uid", value = "用户id", paramType = "query", dataType = "long"),
             @ApiImplicitParam(name = "id", value = "赛事id", paramType = "query", dataType = "long"),
             @ApiImplicitParam(name = "access_token", value = "token", paramType = "query", dataType = "string"),
             @ApiImplicitParam(name = "role", value = "用户身份", paramType = "query", dataType = "string"),
-            @ApiImplicitParam(name = "round", value = "轮次", paramType = "query", dataType = "intger"),
-            @ApiImplicitParam(name = "game", value = "游戏id", paramType = "query", dataType = "intger"),
-            @ApiImplicitParam(name = "page", value = "页码", paramType = "query", dataType = "intger"),
-            @ApiImplicitParam(name = "page_count", value = "每页显示条数", paramType = "query", dataType = "intger")
+            @ApiImplicitParam(name = "round", value = "轮次", paramType = "query", dataType = "integer"),
+            @ApiImplicitParam(name = "game", value = "游戏id", paramType = "query", dataType = "integer"),
+            @ApiImplicitParam(name = "page", value = "页码", paramType = "query", dataType = "integer"),
+            @ApiImplicitParam(name = "page_count", value = "每页显示条数", paramType = "query", dataType = "integer")
     })
     @ApiResponses({
-            @ApiResponse(code = 200,message = ""),
             @ApiResponse(code = 400,message = "客户端请求错误"),
-            @ApiResponse(code = 404,message = ""),
-            @ApiResponse(code = 409,message = ""),
             @ApiResponse(code = 500,message = "服务端内部错误"),
     })
-    @GetMapping("/")
-    public DynamicList getList(@PathVariable long uid, @PathVariable String access_token, @PathVariable int page, @PathVariable int page_count){
+    @GetMapping("/v1/match/dynamic")
+    public DynamicList getList(@PathVariable long uid, @PathVariable long id, @PathVariable String access_token,
+                               @PathVariable String role, @PathVariable int round, @PathVariable int game,
+                               @PathVariable int page, @PathVariable int page_count){
         return new DynamicList();
     }
 }
--- src/main/java/cn/dianjingquan/api/dao/model/Event.java
+++ src/main/java/cn/dianjingquan/api/dao/model/Event.java
@@ -1,6 +1,7 @@
 package cn.dianjingquan.api.dao.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
  * Created by tommy on 2016-12-20.
@@ -20,47 +21,76 @@ public class Event {
     @JsonProperty("postTime")
     protected long postTime;
 
+    @ApiModelProperty(name = "g",value = "游戏名称", readOnly = true)
     @JsonProperty("g")
     protected String g;
 
+    @ApiModelProperty(name = "r",value = "比赛规则", readOnly = true)
     @JsonProperty("r")
     protected String r;
 
+    @ApiModelProperty(name = "n",value = "比赛名称", readOnly = true)
     @JsonProperty("n")
     protected String n;
 
     @JsonProperty("b")
     protected String b;
 
+    @ApiModelProperty(name = "p1",value = "冠军", readOnly = true)
     @JsonProperty("p1")
     protected String p1;
+
+    @ApiModelProperty(name = "p2",value = "亚军", readOnly = true)
     @JsonProperty("p2")
     protected String p2;
+
+    @ApiModelProperty(name = "p3",value = "季军", readOnly = true)
     @JsonProperty("p3")
     protected String p3;
+
+    @ApiModelProperty(name = "p4",value = "主办方", readOnly = true)
     @JsonProperty("p4")
     protected String p4;
+
+    @ApiModelProperty(name = "p5",value = "裁判", readOnly = true)
     @JsonProperty("p5")
     protected String p5;
+
+    @ApiModelProperty(name = "p6",value = "选手A", readOnly = true)
     @JsonProperty("p6")
     protected String p6;
+
+    @ApiModelProperty(name = "p7",value = "选手B", readOnly = true)
     @JsonProperty("p7")
     protected String p7;
+
+    @ApiModelProperty(name = "mr",value = "轮次", readOnly = true)
     @JsonProperty("mr")
     protected String mr;
+
+    @ApiModelProperty(name = "ms",value = "场次", readOnly = true)
     @JsonProperty("ms")
     protected String ms;
+
+    @ApiModelProperty(name = "ma",value = "选手A比分", readOnly = true)
     @JsonProperty("ma")
     protected String ma;
+
+    @ApiModelProperty(name = "mb",value = "选手B比分", readOnly = true)
     @JsonProperty("mb")
     protected String mb;
     @JsonProperty("mt")
     protected String mt;
+
+    @ApiModelProperty(name = "dh",value = "计时时间", readOnly = true)
     @JsonProperty("dh")
     protected String dh;
+
+    @ApiModelProperty(name = "rn",value = "审核未通过原因", readOnly = true)
     @JsonProperty("rn")
     protected String rn;
 
+    @ApiModelProperty(name = "unread",value = "未读消息", readOnly = true)
     @JsonProperty("unread")
     protected boolean unread;
 
